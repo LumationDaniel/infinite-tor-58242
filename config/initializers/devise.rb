eed113cd0e5c50b6f15e0f6b23e702538136d2f5
rails  rub cd <pedro.mansilla35@gmail.com>
2016-06-15 10:41:51 -0600

    Correr

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "me@sourcebender.com"
-
+config.secret_key = '4717ec7a9ceb716e542d521ff78ae68756bca34e1fd04472e3c3b50a7cdfab6a61aeb6b5d8a972299da3c8639ca9796b6995debf22fedac5035a790b1fa02a7a'
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
